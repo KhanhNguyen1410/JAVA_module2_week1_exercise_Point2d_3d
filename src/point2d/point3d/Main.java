f0fdb9a65a3cd1296ee10448ab81c4f616bd5eca
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2020-09-16 16:14:15 +0700

    add

---
+++ src/point2d/point3d/Main.java
@@ -0,0 +1,15 @@
+package point2d.point3d;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2d point2d = new Point2d();
+        System.out.println(point2d);
+        Point2d point2d2 = new Point2d(9,10);
+        System.out.println(point2d2);
+
+        Point3d point3d = new Point3d();
+        System.out.println(point3d);
+        Point3d point3d2 = new Point3d(8,9,10);
+        System.out.println(point3d2);
+    }
+}
